8487632e71678f29f360b2fb43720898e6a3e6b5
Tyler Hasenoehrl <tyler.hasenoehrl@01614-MBP15S.local>
2018-09-17 14:32:08 -0700

    add link to the page

--- public/js/results.js
+++ public/js/results.js
@@ -1,7 +1,7 @@
 function addrow (data) {
   let altText = '<td>' + data.altText + '</td>'
   let imgSrc = '<td class="thumbnail"><img src="' + data.imgSrc + '"/></td>'
-  let pageURL = '<td>' + data.pageURL + '</td>'
+  let pageURL = '<td><a href="' + data.pageURL + '" target="_blank">' + data.pageURL + '</a></td>'
   let row = '<tr>'+ imgSrc + altText + pageURL + '</tr>' 
   let table = $('#results .table tbody')
   $(table).append(row)
